a821b6d43c6ece75f0aaa968619b8c0c7e06b4bc
John Pang <john@pijin.co.uk>
2019-12-10 18:48:53 +0000

    Day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,11 +1,16 @@
 fun main() {
     val range = 130254..678275
-    val valid = range.filter { satisfiesCriteria(it) }
-    val answer = valid.size
-    println(answer)
+
+    val answer1 = range.filter { satisfiesCriteria(it) }.size
+    println(answer1)
+
+    val answer2 = range.filter { satisfiesCriteria2(it) }.size
+    println(answer2)
 }
 
 fun satisfiesCriteria(x: Int): Boolean = incrementCriteria(x) && adjacentCriteria(x)
+
+fun satisfiesCriteria2(x: Int): Boolean = incrementCriteria(x) && doubles(x).isNotEmpty()
 
 fun adjacentCriteria(x: Int): Boolean {
     val chars = x.toString().toCharArray()
@@ -15,4 +20,16 @@ fun adjacentCriteria(x: Int): Boolean {
 fun incrementCriteria(x: Int): Boolean {
     val ints = x.toString().map { it.toInt() }
     return (0..ints.size - 2).all { ints[it] <= ints[it + 1] }
+}
+
+fun doubles(x: Int): List<String> {
+    val chars = x.toString().toCharArray()
+    val multiples: List<String> = chars.fold(emptyList()) { acc, c ->
+        when {
+            acc.isEmpty() -> listOf(c.toString())
+            acc.last().last() == c -> acc.take(acc.size - 1) + (acc.last() + c)
+            else -> acc + c.toString()
+        }
+    }
+    return multiples.filter { it.length == 2}
 }
